38bb03888fd0008dccadfeb9784408f5cd01cec6
Anshul Upadhyay <upadhyayanshul9161@gmail.com>
2025-11-25 10:47:00 +0530

    Loggin and logout with authmiddleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser, loggedOutUser } from "../controllers/user.controller.js";
 import {upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -17,5 +18,12 @@ router.route("/register").post(
     ]),
     registerUser
 );
+
+router.route("/login").post(loginUser)
+
+//Secured Routes
+router.route("/logout").post(verifyJWT, loggedOutUser)
+
+
 
 export default router;
